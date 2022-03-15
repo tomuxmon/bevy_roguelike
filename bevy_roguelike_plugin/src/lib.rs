778948651796aa7db32241adc6b6d429182236a1
tomuxmon <tomas.sql@protonmail.com>
2022-03-15 21:13:53 +0200

    lib and bin scafold

---
+++ bevy_roguelike_plugin/src/lib.rs
@@ -0,0 +1,9 @@
+use bevy::prelude::*;
+
+pub struct RoguelikePlugin;
+
+impl Plugin for RoguelikePlugin {
+    fn build(&self, app: &mut App) {
+        println!("nothing to see here yet");
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use bevy::prelude::*;
+use bevy_roguelike_plugin::RoguelikePlugin;
+// TODO: only in debug
+use bevy_inspector_egui::WorldInspectorPlugin;
+
+fn main() {
+    let mut app = App::new();
+    app.insert_resource(WindowDescriptor {
+        title: "Mine Sweeper!".to_string(),
+        width: 700.,
+        height: 800.,
+        ..Default::default()
+    })
+    .add_plugins(DefaultPlugins)
+    .add_plugin(RoguelikePlugin {})
+    .add_plugin(WorldInspectorPlugin::new())
+    .add_startup_system(camera_setup);
+
+    app.run();
+}
+
+fn camera_setup(mut cmd: Commands) {
+    cmd.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
