8dba41bc04c284d9c754ca499468dd2c414963ec
tomuxmon <tomas.sql@protonmail.com>
2022-04-13 16:01:20 +0300

    paralel turns compute

--- bevy_roguelike_plugin/src/systems/turns.rs
+++ bevy_roguelike_plugin/src/systems/turns.rs
@@ -1,6 +1,7 @@
 use crate::{components::*, events::*};
 use bevy::log;
 use bevy::prelude::*;
+use bevy::tasks::AsyncComputeTaskPool;
 
 pub fn apply_hp_modify(
     mut cmd: Commands,
@@ -18,23 +19,25 @@ pub fn apply_hp_modify(
         }
     }
 }
-pub fn gather_action_points(mut actors: Query<(&mut Capability, &mut TurnState)>) {
-    for (mut cp, mut ts) in actors
-        .iter_mut()
-        .filter(|(_, ts)| **ts == TurnState::Collect)
-    {
-        *ts = if cp.ap_current_add() > cp.ap_turn_ready_to_act() {
-            TurnState::Act
-        } else {
-            // NOTE: not yet ready to perform turn.
-            // skip this turn.
-            TurnState::End
-        };
-    }
+pub fn gather_action_points(
+    pool: Res<AsyncComputeTaskPool>,
+    mut actors: Query<(&mut Capability, &mut TurnState)>,
+) {
+    actors.par_for_each_mut(&*pool, 1000, |(mut cp, mut ts)| {
+        if *ts == TurnState::Collect {
+            *ts = if cp.ap_current_add() > cp.ap_turn_ready_to_act() {
+                TurnState::Act
+            } else {
+                // NOTE: not yet ready to perform turn.
+                // skip this turn.
+                TurnState::End
+            };
+        }
+    });
 }
-pub fn turn_end_now_gather(mut actors: Query<&mut TurnState>) {
+pub fn turn_end_now_gather(pool: Res<AsyncComputeTaskPool>, mut actors: Query<&mut TurnState>) {
     if actors.iter().all(|ts| *ts == TurnState::End) {
-        actors.for_each_mut(|mut ts| {
+        actors.par_for_each_mut(&*pool, 1000, |mut ts| {
             *ts = TurnState::Collect;
         });
     }
