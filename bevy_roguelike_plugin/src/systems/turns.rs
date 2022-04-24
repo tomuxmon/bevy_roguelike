da17ae91eda7d437d9b4a84064e90472b8674778
tomuxmon <tomas.sql@protonmail.com>
2022-04-24 07:06:59 +0300

    using wayland instead of x11, removing unused configs from launch.json, using bevy_easings v 0.7 instead of local, tiny rewamp of act system (still not correct)

--- bevy_roguelike_plugin/src/systems/turns.rs
+++ bevy_roguelike_plugin/src/systems/turns.rs
@@ -6,7 +6,7 @@ use bevy::tasks::AsyncComputeTaskPool;
 use bevy::utils::HashMap;
 
 pub fn act(
-    mut actors: Query<(Entity, &Team, &Capability, &Vector2D)>,
+    actors: Query<(Entity, &Team, &Capability, &Vector2D)>,
     mut act_reader: EventReader<ActEvent>,
     mut hp_writer: EventWriter<ModifyHPEvent>,
     mut ap_writer: EventWriter<SpendAPEvent>,
@@ -20,33 +20,28 @@ pub fn act(
         (IVec2::new(1, 0), 900),
         (IVec2::new(0, 0), 451), // stay put - skip turn
     ]);
-
-    let ocupied = HashMap::from_iter(actors.iter().map(|(e, t, _, p)| (**p, (e, *t))));
-
     for e in act_reader.iter() {
-        if let Ok((_, team, cp, pt)) = actors.get_mut(e.id) {
-            // let delta = Vector2D::from(e.delta);
-            // if delta != Vector2D::minmin() {}
-            let mut cost = delta_costs[&e.delta];
+        if e.delta == IVec2::new(0, 0) {
+            ap_writer.send(SpendAPEvent::new(e.id, delta_costs[&e.delta]));
+            continue;
+        }
+        if let Ok((_, team, cp, pt)) = actors.get(e.id) {
             let dest = **pt + e.delta;
             if !map.is_in_bounds(dest) || map[dest] != Tile::Floor {
                 continue;
             }
-            let other = ocupied.get(&dest);
-            // NOTE: can not move into a tile ocupied by a team mate
-            if other.is_some() && other.unwrap().1 == *team && e.delta != IVec2::new(0, 0) {
-                continue;
-            }
-            // TODO: instead of 'delta != ..' check on is_same_id
-            if other.is_some() && e.delta != IVec2::new(0, 0) {
-                cost = cp.attack_cost();
-                hp_writer.send(ModifyHPEvent::new(other.unwrap().0, -cp.attack_damage()));
-            } else {
-                if e.delta != IVec2::new(0, 0) {
-                    move_writer.send(MoveEvent::new(e.id, dest));
+            if let Some((o_id, o_team, _, _)) = actors.iter().find(|(_, _, _, pt)| ***pt == dest) {
+                if o_team == team {
+                    // NOTE: can not move into a tile ocupied by a team mate
+                    continue;
+                } else {
+                    ap_writer.send(SpendAPEvent::new(e.id, cp.attack_cost()));
+                    hp_writer.send(ModifyHPEvent::new(o_id, -cp.attack_damage()));
                 }
+            } else {
+                ap_writer.send(SpendAPEvent::new(e.id, delta_costs[&e.delta]));
+                move_writer.send(MoveEvent::new(e.id, dest));
             }
-            ap_writer.send(SpendAPEvent::new(e.id, cost));
         }
     }
 }
@@ -88,6 +83,8 @@ pub fn do_move(
 ) {
     for e in move_reader.iter() {
         if let Ok((mut pt, mut tr, mut fov)) = actors.get_mut(e.id) {
+            // TODO: mark / unmark ocupied tiles
+
             let z = tr.translation.z;
             tr.translation = map_options.to_world_position(e.destination).extend(z);
             *pt = Vector2D::from(e.destination);
